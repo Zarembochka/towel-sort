f8360566e1e29c6f12aabf2663a52ac6fc251fb5
Luba Hadunova <luba.godunova2018@gmail.com>
2023-06-29 19:25:11 +0200

    fix: fix solution in case of null array

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+  if (!Array.isArray(matrix)) {
+    return [];
+  }
 
   matrix.map((element, index) => {
     if (index % 2 !== 0) {
